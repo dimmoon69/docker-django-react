1e5d0ba4c88700a6e950ec38233c4e2ccd76590c
Andrew Bondarenko <adementev78@gmail.com>
2022-10-12 11:35:14 +0300

    foodgram v1.0

---
+++ backend/users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+# from .models import User
+
+User = get_user_model()
+admin.site.empty_value_display = '--Пусто--'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_superuser')
+    search_fields = ('username', 'email')
+    list_filter = ('username', 'email')
